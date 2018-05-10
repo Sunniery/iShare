0d6c3bca4a57b9042c0ff434c0b007501cbc7bb6
Starrier <1342878298@qq.com>
2018-05-10 17:33:57 +0800

    modify iShare

--- src/main/ishare.sql
+++
@@ -1,9 +0,0 @@
-/* create database */
- CREATE DATABASE ishare;
-
-/* create table t_user */
-CREATE TABLE t_user(
-  id int PRIMARY KEY AUTO_INCREMENT,
-  userName VARCHAR(18),
-  password VARCHAR(20)
-);
---
+++ src/main/java/org/starrier/ishare/dao/StudentDao.java
@@ -0,0 +1,16 @@
+package org.starrier.ishare.dao;
+
+
+
+import org.starrier.ishare.entity.Student;
+
+import java.util.List;
+
+public interface StudentDao {
+    int getTotal();
+    void addStudent(Student student);
+    void deleteStudent(int id);
+    void updateStudent(Student student);
+    Student getStudent(int id);
+    List<Student> list(int start, int count);
+}
